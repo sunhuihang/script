bab5af1dedfd3bac4880d93f2b5cb0858e3d89ca
sunhuihang <56792866+sunhuihang@users.noreply.github.com>
2024-02-01 18:08:28 +0800

    Create conda使用.sh

---
+++ 常用代码/conda使用.sh
@@ -0,0 +1,8 @@
+
+#先更新 ，后面安装才顺利
+conda update -n base -c defaults conda
+
+需要安装的库
+matplotlib cartopy gdal netcdf4 eccodes xarray pandas cfgrib h5py hdf4 hdf5 jupyter onnx pillow(PIL) scipy xesmf scikit-learn scikit-image pytorch-lightning tensorboard tqdm torchinfo
+
+pickle是python自带 不用装 ，PIL的库名叫pillow
