6816cbf7823a87d9db90f41d4c8c2fe015cb43a6
shhh <56792866+sunhuihang@users.noreply.github.com>
2024-11-07 14:27:43 +0800

    Update python库的使用经验.py

--- 常用代码/python库的使用经验.py
+++ 常用代码/python库的使用经验.py
@@ -1907,6 +1907,53 @@ print(t2) #tensor([[ 0.9093,  0.0927,  0.0043, -0.4161,  0.9957,  1.0000]])
 
 
 
+#### 输入dayofyear 或者 hour 进行编码
+class TimestepEmbedder(nn.Module):
+    """
+    Embeds scalar timesteps into vector representations.
+    """
+
+    def __init__(self, hidden_size, frequency_embedding_size=512):
+        super().__init__()
+        self.mlp = nn.Sequential(
+            nn.Linear(frequency_embedding_size, hidden_size, bias=True),
+            nn.SiLU(),
+            nn.Linear(hidden_size, hidden_size, bias=True),
+        )
+        self.frequency_embedding_size = frequency_embedding_size
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        for m in self.mlp:
+            
+            (m)
+
+    @staticmethod
+    def timestep_embedding(t, dim, max_period=10000):
+        # def timestep_embedding(t, dim, max_period=480):
+        """
+        Create sinusoidal timestep embeddings.
+        :param t: a 1-D Tensor of N indices, one per batch element.
+                          These may be fractional.
+        :param dim: the dimension of the output.
+        :param max_period: controls the minimum frequency of the embeddings.
+        :return: an (N, D) Tensor of positional embeddings.
+        """
+        # https://github.com/openai/glide-text2im/blob/main/glide_text2im/nn.py
+        half = dim // 2
+        freqs = torch.exp(
+            -math.log(max_period) * torch.arange(start=0, end=half, dtype=torch.float32) / half
+        ).to(device=t.device)
+        args = t[:, None].float() * freqs[None]
+        embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)
+        if dim % 2:
+            embedding = torch.cat([embedding, torch.zeros_like(embedding[:, :1])], dim=-1)
+        return embedding
+
+    def forward(self, t):
+        t_freq = self.timestep_embedding(t, self.frequency_embedding_size)
+        t_emb = self.mlp(t_freq)
+        return t_emb
 
 
 
