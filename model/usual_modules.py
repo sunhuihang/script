a17da2eed3e5d1fcf549413b0aee7d7cc66daa19
shhh <56792866+sunhuihang@users.noreply.github.com>
2026-03-11 17:19:28 +0800

    Implement Depthwise Separable Convolution and CBAM

---
+++ model/usual_modules.py
@@ -0,0 +1,85 @@
+# DWSC 深度可分离卷积，是正常卷积的轻量化替代
+class DepthwiseSeparableConv(nn.Module):
+    def __init__(self, in_channels, output_channels, kernel_size, padding=0, kernels_per_layer=1):
+        super(DepthwiseSeparableConv, self).__init__()
+        # In Tensorflow DepthwiseConv2D has depth_multiplier instead of kernels_per_layer
+        self.depthwise = nn.Conv2d(
+            in_channels,
+            in_channels * kernels_per_layer,
+            kernel_size=kernel_size,
+            padding=padding,
+            groups=in_channels,
+        )
+        self.pointwise = nn.Conv2d(in_channels * kernels_per_layer, output_channels, kernel_size=1)
+
+    def forward(self, x):
+        x = self.depthwise(x)
+        x = self.pointwise(x)
+        return x
+
+class DoubleDSConv(nn.Module):
+    """(convolution => [BN] => ReLU) * 2 堆叠时一般都是conv norm act"""
+    
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.double_ds_conv = nn.Sequential(
+            DepthwiseSeparableConv(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            DepthwiseSeparableConv(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.double_ds_conv(x)
+
+
+  # CBAM 通道空间注意力
+  class ChannelAttention(nn.Module):
+    def __init__(self, input_channels, reduction_ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.input_channels = input_channels
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        #  https://github.com/luuuyi/CBAM.PyTorch/blob/master/model/resnet_cbam.py
+        #  uses Convolutions instead of Linear
+        self.MLP = nn.Sequential(
+            Flatten(),
+            nn.Linear(input_channels, input_channels // reduction_ratio),
+            nn.ReLU(),
+            nn.Linear(input_channels // reduction_ratio, input_channels),
+        )
+    def forward(self, x):
+        # Take the input and apply average and max pooling
+        avg_values = self.avg_pool(x)
+        max_values = self.max_pool(x)
+        out = self.MLP(avg_values) + self.MLP(max_values)
+        scale = x * torch.sigmoid(out).unsqueeze(2).unsqueeze(3).expand_as(x)
+        return scale
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+        assert kernel_size in (3, 7), "kernel size must be 3 or 7"
+        padding = 3 if kernel_size == 7 else 1
+        self.conv = nn.Conv2d(2, 1, kernel_size=kernel_size, padding=padding, bias=False)
+        self.bn = nn.BatchNorm2d(1)
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        out = torch.cat([avg_out, max_out], dim=1)
+        out = self.conv(out)
+        out = self.bn(out)
+        scale = x * torch.sigmoid(out)
+        return scale
+class CBAM(nn.Module):
+    def __init__(self, input_channels, reduction_ratio=16, kernel_size=7):
+        super(CBAM, self).__init__()
+        self.channel_att = ChannelAttention(input_channels, reduction_ratio=reduction_ratio)
+        self.spatial_att = SpatialAttention(kernel_size=kernel_size)
+
+    def forward(self, x):
+        out = self.channel_att(x)
+        out = self.spatial_att(out)
+        return out
+
